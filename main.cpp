d29c3d48ace3800dce4ffb3d606575af08625a2b
Chris Morrison <gnosticist@protonmail.com>
2021-02-28 21:42:47 +0000

    Documentation work

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+/*
+ * Copyright (c) 2020 Chris Morrison
+ *
+ * Filename: main.cpp
+ * Created on Thu Dec 24 2020
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <string>
 #include <list>
 #include <bitset>
 #include <boost/program_options.hpp>
-#include <duplicate_files_scanner.hpp>
+#include "oasis/duplicate_files_scanner.hpp"
+#include "oasis/duplicate_file_set.hpp"
 
 #define OP_SUMMARY 0
 #define OP_DELETE  1
@@ -17,8 +38,8 @@
 
 
 std::string search_directory;
-bool recurse;
-bool follow_symlinks;
+bool recurse = false;
+bool follow_symlinks = false;
 size_t min_size = 0;
 size_t max_size = SIZE_MAX;
 bool skip_hidden = false;
@@ -35,6 +56,7 @@ bool name_sort = false;
 bool size_sort = false;
 bool descending = false;
 bool link_files;
+boost::mutex conlock;
 
 template<typename T>
 int perform_operation(const std::string& search_dir, const std::bitset<3>& operation);
@@ -61,38 +83,86 @@ inline void print_help(const boost::program_options::options_description &desc)
 
 void scan_started_callback(const boost::filesystem::path& search_path)
 {
-    std::cout << "Starting scan of directory " << search_path << "...\n";
+    std::cout << "Starting scan of directory " << search_path.string() << "...\n";
 }
 
 void scan_progress_callback(const boost::filesystem::path& search_path, uintmax_t files_encountered, uintmax_t sets_found)
 {
     std::locale user_locale("");
     std::cout.imbue(user_locale);
-    std::cout << "Searching path " << search_path << "\n";
-    std::cout << "Files encountered: " << files_encountered << ", duplicate sets found: " << sets_found << "\n";
+    std::cout << "\033[KSearching path " << search_path.string() << ", files encountered: " << files_encountered << ", duplicate sets found: " << sets_found << "\r" << std::flush;
 }
 
 void scan_completed_callback(const boost::filesystem::path& search_path, uintmax_t examined, uintmax_t duplicate_file_count, uintmax_t sets_found, uintmax_t space_occupied)
 {
     std::locale user_locale("");
     std::cout.imbue(user_locale);
-    std::cout << "Files examined: " << examined << ", duplicate sets found: " << sets_found << std::endl;
+    std::cout << "\033[KFiles examined: " << examined << ", duplicate sets found: " << sets_found << std::endl;
 }
 
 void scan_error_callback(const boost::filesystem::path& search_path, const boost::filesystem::path& error_file, const std::error_condition& error)
 {
+    conlock.lock();
     if (error_file.empty())
     {
-        std::cerr << "An error occurred while scanning files and directories in " << search_path << " - " << error.message() << std::endl;
+        std::cerr << "\033[31;1mAn error occurred while scanning files and directories in " << search_path << " - " << error.message() << "\033[0m" << std::endl;
     }
     else
     {
-        std::cerr << "An error occurred while scanning the file or directory " << error_file << " - " << error.message() << std::endl;
+        std::cerr << "\033[31;1mAn error occurred while scanning the file or directory " << error_file << " - " << error.message() << "\033[0m" << std::endl;
     }
+    conlock.unlock();
 }
 
 int main(int argc, char **argv)
 {
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test1("thisisthehash1");
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test2(L"thisisthehash2");
+    const char *t1 = "thisisthehash3";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test3(t1);
+    std::string t2 = "thisisthehash4";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test4(t2);
+    const wchar_t *t3 = L"thisisthehash5";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test5(t3);
+    std::wstring t4 = L"thisisthehash6";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test6(t4);
+    const char32_t *t5 = U"thisisthehash7";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test7(t5);
+    std::u32string t6 = U"thisisthehash8";
+    oasis::filesystem::duplicate_file_set<oasis::filesystem::sort_by_filename> test8(t6);
+
+    if (test1 != test2) std::cout << "Test 1/29: Inequality test holds\n";
+    test1 = test2;
+    if (test1 == test2) std::cout << "Test 2/29: Equality test holds following assignment\n";
+    if (test3 == t1) std::cout << "Test 3/29: Equality test holds\n";
+    if (test3 == "thisisthehash3") std::cout << "Test 4/8: Equality test holds\n";
+    if (test4 == t2) std::cout << "Test 5/29: Equality test holds\n";
+    if (test5 == t3) std::cout << "Test 6/29: Equality test holds\n";
+    if (test5 == L"thisisthehash5") std::cout << "Test 7/8: Equality test holds\n";
+    if (test6 == t4) std::cout << "Test 8/29: Equality test holds\n";
+    if (test7 == t5) std::cout << "Test 9/29: Equality test holds\n";
+    if (test8 == t6) std::cout << "Test 10/29: Equality test holds\n";
+    if (test3 > test1) std::cout << "Test 11/29: Greater than test holds\n";
+    if (test6 > t1) std::cout << "Test 12/29: Greater than test holds\n";
+    if (test6 > "thisisthehash3") std::cout << "Test 13/29: Greater than test holds\n";
+    if (test6 > L"thisisthehash3") std::cout << "Test 14/29: Greater than test holds\n";
+    if (test6 > U"thisisthehash3") std::cout << "Test 15/29: Greater than test holds\n";
+    if (test8 > t2) std::cout << "Test 16/29: Greater than test holds\n";
+    if (test8 > t3) std::cout << "Test 17/29: Greater than test holds\n";
+    if (test8 > t4) std::cout << "Test 18/29: Greater than test holds\n";
+    if (test8 > t5) std::cout << "Test 19/29: Greater than test holds\n";
+    if (test7 < t6) std::cout << "Test 20/29: Less than test holds\n";
+    if (test1 < "thisisthehash3") std::cout << "Test 21/29: Less than test holds\n";
+    if (test1 < L"thisisthehash3") std::cout << "Test 22/29: Less than test holds\n";
+    if (test1 < U"thisisthehash3") std::cout << "Test 23/29: Less than test holds\n";
+    if (test1 < t1) std::cout << "Test 24/29: Less than test holds\n";
+    if (test1 < t2) std::cout << "Test 25/29: Less than test holds\n";
+    if (test1 < t3) std::cout << "Test 26/29: Less than test holds\n";
+    if (test1 < t4) std::cout << "Test 27/29: Less than test holds\n";
+    if (test1 < t5) std::cout << "Test 28/29: Less than test holds\n";
+    if (test1 < t6) std::cout << "Test 29/29: Less than test holds\n";
+
+    return 0;
     // Declare the hidden (positional) options.
     boost::program_options::options_description hidden("Hidden Options");
     hidden.add_options()
@@ -113,7 +183,7 @@ int main(int argc, char **argv)
             ("summarise,m", boost::program_options::bool_switch(&print_summary), "Show a summary of the duplicate file information.")
             ("delete,d", boost::program_options::bool_switch(&delete_files), "Print a list of the duplicate files in a set and prompt for one to keep, the remainder will be deleted.")
             ("link,l", boost::program_options::bool_switch(&link_files), "Print a list of the duplicate files in a set and prompt for one to keep, the remainder will be deleted and replaced with a symbolic link to the file that has been kept.")
-            ("noprompt,N", boost::program_options::bool_switch(&no_prompt), "Together with --delete, preserve the first file in each set of duplicates and delete the rest without prompting.")
+            ("noprompt,N", boost::program_options::bool_switch(&no_prompt), "Together with --delete or --link, preserve the first file in each set of duplicates and delete or link to the rest without prompting.")
             ("creation-time,c", boost::program_options::bool_switch(&creation_sort), "List the duplicate files, in each set, in order of their creation time.")
             ("last-read-time,a", boost::program_options::bool_switch(&last_read_sort), "List the duplicate files, in each set, in order of their last read time.")
             ("last-write-time,M", boost::program_options::bool_switch(&last_write_sort), "List the duplicate files, in each set, in order of their last write time.")
@@ -121,8 +191,8 @@ int main(int argc, char **argv)
             ("size,b", boost::program_options::bool_switch(&size_sort), "List the duplicate files, in each set, in order of their size.")
             ("descending", boost::program_options::bool_switch(&descending), "Files will be sorted in descending order.");
 
-   /* try
-    { */
+    try
+    {
         // Set up positional options
         boost::program_options::positional_options_description p;
         p.add("search-directory", 1);
@@ -195,12 +265,12 @@ int main(int argc, char **argv)
         if (sort_opts.test(SORT_FILE_SIZE)) return perform_operation<oasis::filesystem::sort_by_file_size>(search_directory, op_flags);
 
         return 0;
-   /* }
+    }
     catch (const std::exception &ex)
     {
         std::cerr << "Fatal error: " << ex.what() << std::endl;
         return 1;
-    } */
+    }
 }
 
 template<typename T>
@@ -228,32 +298,19 @@ int perform_operation(const std::string& search_dir, const std::bitset<3>& opera
         std::cout << "  Files examined:        " << scanner.files_examined() << "\n";
         std::cout << "  Duplicate files found: " << scanner.file_count() << "\n";
         std::cout << "  Duplicate sets found:  " << scanner.set_count() << "\n";
-        std::cout << "  Space occupied:        " << scanner.space_occupied() << " bytes." << std::endl;
-        //return 0;
-    }
+        std::cout << "  Space occupied:        " << oasis::storage_formatter(scanner.space_occupied()) << std::endl;
 
-    std::cout << "\nTesting forward iterator:\n\n";
+        return 0;
+    }
 
     size_t i = 1;
     for (const auto& ds : scanner)
     {
-        std::cout << "Set " << i++ << "\n";
+        i = 1;
         for (const auto& dp : ds)
         {
-            std::cout << " " << dp << "\n";
-        }
-    }
-
-    std::cout << "\nTesting reverse iterator:\n\n";
-
-    i--;
-    for (auto it = scanner.rbegin(); it != scanner.rend(); it++)
-    {
-        const auto& ds = *it;
-        std::cout << "Set " << i-- << "\n";
-        for (const auto& dp : ds)
-        {
-            std::cout << " " << dp << "\n";
+            std::cout << std::left << std::setfill(' ') << std::setw(10) << i << " " << dp;
+            if (print_size) std::cout << " (" << boost::filesystem::file_size(dp) << ")";
         }
     }
 
